effb02840be4628b22059ab502b65bfeb2ff5d3b
Naya Willis <greedybrain18@gmail.com>
2020-04-15 22:05:41 -0400

    refactored application controller helper methods

--- backend_api/app/controllers/application_controller.rb
+++ backend_api/app/controllers/application_controller.rb
@@ -1,18 +1,18 @@
 class ApplicationController < ActionController::API
      include AbstractController::Helpers
-     helper_method :logged_in?, :current_layman, :current_opp_layman
+     helper_method :current_layman, :current_opp_layman
 
-     def current_layman
-          @current_layman = Layman.find(Layman.all.sample.id)
-     end
+     # def current_layman
+     #      @current_layman = Layman.find(Layman.all.sample.id)
+     # end
 
-     def current_opp_layman
-          @current_opp_layman = Layman.find(Layman.all.sample.id)
-     end
+     # def current_opp_layman
+     #      @current_opp_layman = Layman.find(Layman.all.sample.id)
+     # end
 
-     def logged_in? 
-          !session[:layman_id].nil?
-     end
+     # def logged_in? 
+     #      !session[:layman_id].nil?
+     # end
 
      # def authenticate_user 
      #      logged_in? && current_layman

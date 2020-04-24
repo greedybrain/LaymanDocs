a221ceffac6f800b7bddb4d31155c1848330f31f
Naya Willis <greedybrain18@gmail.com>
2020-04-24 09:52:07 -0400

    refac sessions contr and routes/ action changes

--- backend_api/app/controllers/sessions_controller.rb
+++ backend_api/app/controllers/sessions_controller.rb
@@ -1,18 +1,29 @@
 class SessionsController < ApplicationController
 
-     def create
+     def logged_in
           layman = Layman.find_by(email: params[:email])
           if layman&.authenticate(params[:password])
                session[:layman_id] = layman.id
-               render json: LaymanSerializer.new(layman).serializable_hash
+               render json: { 
+                    status: :created,
+                    logged_in: true,
+                    layman: LaymanSerializer.new(layman).serializable_hash
+               }
           else
-               render json: { message: "Incorrect email or password" }
+               render json: {
+                    message: "Incorrect email or password",
+                    status: 401 
+               }
           end
      end
 
-     def destroy 
+     def logout 
           session.delete(:layman_id)
-          render json: { message: "You successfully logged out" }
+          render json: { 
+               status: 200,
+               logged_out: true,
+               message: "You successfully logged out" 
+          }
      end
 
 end
--- backend_api/config/routes.rb
+++ backend_api/config/routes.rb
@@ -22,12 +22,12 @@ Rails.application.routes.draw do
   # Signup, Login, and Logout routing
   resources :sessions, except: %i[create destroy]
   post "/signup", to: "laymen#create", as: "signup"
-  post "/login", to: "sessions#create", as: "login"
-  delete "/logout", to: "sessions#destroy", as: "logout"
+  post "/login", to: "sessions#logged_in", as: "login"
+  delete "/logout", to: "sessions#logout", as: "logout"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
 # ****** NOTE ******
 # Only the elbaorator can delete their elaborations DIRECTLY 
-# When a laymen deletes a question/post, then all elaborations go with it
+# When a layman deletes a question/post, then all elaborations go with it

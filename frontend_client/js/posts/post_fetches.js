5fc2e8694dd7ee2f870ad6a62af1974ab486ec98
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-04-25 08:37:19 -0400

    added script tag to index.html for post-fetch.js

--- frontend_client/js/posts/post_fetches.js
+++ frontend_client/js/posts/post_fetches.js
@@ -1,10 +1,13 @@
 class PostFetch {
      constructor(urlField, pasteInfoField, data = null, options = null) {
-          this.urlField = document.querySelector("")
+          this.urlField = document.querySelector("input[name=url]")
+          this.pasteInfoField = document.querySelector("input[name=pasted-info]")
           this.data = data
           this.options = options
      }
      get handleUrlPasteEvent() {
+          this.urlField.addEventListener('paste', e => {
 
+          })
      }
 }

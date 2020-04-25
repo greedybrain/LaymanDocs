8f91dbb5631f3293a99fb6bbf10de3da98d9764a
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-04-24 22:03:47 -0400

    created elements that forms the post cards header

--- frontend_client/js/posts/post_create.js
+++ frontend_client/js/posts/post_create.js
@@ -6,14 +6,32 @@ class Post {
   }
   createHeader() {
     // top level parent elements 
-    const header = document.createElement('div')
-    const topicWrapper = document.createElement('div')
-    const menuWrapper = document.createElement('div')
+    const headerDiv = document.createElement('div')
+    headerDiv.classList.add("card-header")
+    const topicWrapperDiv = document.createElement('div')
+    topicWrapperDiv.classList.add("heading")
+    const menuWrapperDiv = document.createElement('div')
+    menuWrapperDiv.classList.add("menu")
 
-    const headerTopic = document.createElement('h2')
-    const topicContent = document.createElement('a')
-    topicContent.textContent = this.topic
-    topicContent.setAttribute('href', `${this.url}`)
-    headerTopic.appendChild(topicContent)
+    const headerTopicH2tag = document.createElement('h2')
+    const topicContentAtag = document.createElement('a')
+    topicContentAtag.textContent = this.topic
+    topicContentAtag.setAttribute('href', `${this.url}`)
+    headerTopicH2tag.appendChild(topicContentAtag)
+
+    topicWrapperDiv.appendChild(headerTopicH2tag)
+
+    for (let x = 0; x < 3; x++) {
+      let divDot = document.createElement('div')
+      divDot.classList.add("dot")
+      menuWrapperDiv.append(divDot)
+    }
+
+    headerDiv.append(topicWrapperDiv, menuWrapperDiv)
+    return headerDiv
   }
 }
+
+let new_post = new Post("Rails", "http://www.google.com", "Nice work Johnny")
+
+new_post.createHeader()
